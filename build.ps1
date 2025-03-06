7bffaa02ff34bbc7db0d5f26962f06d12853f20b
awtnb <arikaebanataw@gmail.com>
2025-03-06 16:12:04 +0900

    update builder

--- build.ps1
+++ build.ps1
@@ -1,12 +1,11 @@
-go build
+$d = $args[0]
+if ($d.length -lt 1) {
+    Write-Host "Specify directory to copy executable."
+}
+else {
+    go build
 
-if ($LASTEXITCODE -eq 0) {
-    if (Test-Path .\.env) {
-        $d = (Get-Content .\.env -Raw).Trim()
-        if ($d.Length -lt 1) {
-            $d = $env:USERPROFILE | Join-Path -ChildPath "tools\bin"
-        }
-        $d = [System.Environment]::ExpandEnvironmentVariables($d)
+    if ($LASTEXITCODE -eq 0) {
         if (-not (Test-Path $d -PathType Container)) {
             New-Item -Path $d -ItemType Directory
         }
@@ -21,9 +20,6 @@ if ($LASTEXITCODE -eq 0) {
         }
     }
     else {
-        ".env not found!" | Write-Host -ForegroundColor Magenta
+        "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
     }
-}
-else {
-    "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
 }
